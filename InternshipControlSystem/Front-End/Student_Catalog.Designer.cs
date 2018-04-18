5d36921786e961a24b0ece1187bfe09d40563063
Tobe Franco <tobe117@outlook.es>
2018-04-18 04:48:00 -0500

    Added the GUI for the Students Catalog

--- InternshipControlSystem/Front-End/Student_Catalog.Designer.cs
+++ InternshipControlSystem/Front-End/Student_Catalog.Designer.cs
@@ -28,11 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlStudentOverview = new System.Windows.Forms.Panel();
+            this.btnStudentDetails = new System.Windows.Forms.Button();
+            this.lblPeriodHolder = new System.Windows.Forms.Label();
+            this.lblPeriod = new System.Windows.Forms.Label();
+            this.lblProyectNameHolder = new System.Windows.Forms.Label();
+            this.lblProyectName = new System.Windows.Forms.Label();
+            this.gpbStudentOverview = new System.Windows.Forms.GroupBox();
+            this.lblNoControlHolder = new System.Windows.Forms.Label();
+            this.lblNoControl = new System.Windows.Forms.Label();
+            this.lblStudentNameHolder = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.pbStudentOverview = new System.Windows.Forms.PictureBox();
+            this.lblPnlHeader = new System.Windows.Forms.Label();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.lblStudentId = new System.Windows.Forms.Label();
+            this.txtNoControlFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlStudentOverview.SuspendLayout();
+            this.gpbStudentOverview.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStudentOverview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlStudentOverview
+            // 
+            this.pnlStudentOverview.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pnlStudentOverview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlStudentOverview.Controls.Add(this.btnStudentDetails);
+            this.pnlStudentOverview.Controls.Add(this.lblPeriodHolder);
+            this.pnlStudentOverview.Controls.Add(this.lblPeriod);
+            this.pnlStudentOverview.Controls.Add(this.lblProyectNameHolder);
+            this.pnlStudentOverview.Controls.Add(this.lblProyectName);
+            this.pnlStudentOverview.Controls.Add(this.gpbStudentOverview);
+            this.pnlStudentOverview.Controls.Add(this.pbStudentOverview);
+            this.pnlStudentOverview.Controls.Add(this.lblPnlHeader);
+            this.pnlStudentOverview.Location = new System.Drawing.Point(699, 22);
+            this.pnlStudentOverview.Name = "pnlStudentOverview";
+            this.pnlStudentOverview.Size = new System.Drawing.Size(218, 430);
+            this.pnlStudentOverview.TabIndex = 5;
+            // 
+            // btnStudentDetails
+            // 
+            this.btnStudentDetails.Location = new System.Drawing.Point(15, 379);
+            this.btnStudentDetails.Name = "btnStudentDetails";
+            this.btnStudentDetails.Size = new System.Drawing.Size(191, 31);
+            this.btnStudentDetails.TabIndex = 7;
+            this.btnStudentDetails.Text = "Detalles de Alumno";
+            this.btnStudentDetails.UseVisualStyleBackColor = true;
+            // 
+            // lblPeriodHolder
+            // 
+            this.lblPeriodHolder.AutoSize = true;
+            this.lblPeriodHolder.Location = new System.Drawing.Point(42, 342);
+            this.lblPeriodHolder.Name = "lblPeriodHolder";
+            this.lblPeriodHolder.Size = new System.Drawing.Size(37, 13);
+            this.lblPeriodHolder.TabIndex = 6;
+            this.lblPeriodHolder.Text = "----------";
+            // 
+            // lblPeriod
+            // 
+            this.lblPeriod.AutoSize = true;
+            this.lblPeriod.Location = new System.Drawing.Point(25, 325);
+            this.lblPeriod.Name = "lblPeriod";
+            this.lblPeriod.Size = new System.Drawing.Size(43, 13);
+            this.lblPeriod.TabIndex = 5;
+            this.lblPeriod.Text = "Periodo";
+            // 
+            // lblProyectNameHolder
+            // 
+            this.lblProyectNameHolder.AutoSize = true;
+            this.lblProyectNameHolder.Location = new System.Drawing.Point(39, 305);
+            this.lblProyectNameHolder.Name = "lblProyectNameHolder";
+            this.lblProyectNameHolder.Size = new System.Drawing.Size(37, 13);
+            this.lblProyectNameHolder.TabIndex = 4;
+            this.lblProyectNameHolder.Text = "----------";
+            // 
+            // lblProyectName
+            // 
+            this.lblProyectName.AutoSize = true;
+            this.lblProyectName.Location = new System.Drawing.Point(22, 290);
+            this.lblProyectName.Name = "lblProyectName";
+            this.lblProyectName.Size = new System.Drawing.Size(52, 13);
+            this.lblProyectName.TabIndex = 3;
+            this.lblProyectName.Text = "Proyecto:";
+            // 
+            // gpbStudentOverview
+            // 
+            this.gpbStudentOverview.Controls.Add(this.lblNoControlHolder);
+            this.gpbStudentOverview.Controls.Add(this.lblNoControl);
+            this.gpbStudentOverview.Controls.Add(this.lblStudentNameHolder);
+            this.gpbStudentOverview.Controls.Add(this.lblStudentName);
+            this.gpbStudentOverview.Location = new System.Drawing.Point(15, 159);
+            this.gpbStudentOverview.Name = "gpbStudentOverview";
+            this.gpbStudentOverview.Size = new System.Drawing.Size(191, 106);
+            this.gpbStudentOverview.TabIndex = 2;
+            this.gpbStudentOverview.TabStop = false;
+            this.gpbStudentOverview.Text = "Estudiante";
+            // 
+            // lblNoControlHolder
+            // 
+            this.lblNoControlHolder.AutoSize = true;
+            this.lblNoControlHolder.Location = new System.Drawing.Point(27, 74);
+            this.lblNoControlHolder.Name = "lblNoControlHolder";
+            this.lblNoControlHolder.Size = new System.Drawing.Size(37, 13);
+            this.lblNoControlHolder.TabIndex = 3;
+            this.lblNoControlHolder.Text = "----------";
+            // 
+            // lblNoControl
+            // 
+            this.lblNoControl.AutoSize = true;
+            this.lblNoControl.Location = new System.Drawing.Point(7, 59);
+            this.lblNoControl.Name = "lblNoControl";
+            this.lblNoControl.Size = new System.Drawing.Size(63, 13);
+            this.lblNoControl.TabIndex = 2;
+            this.lblNoControl.Text = "No. Control:";
+            // 
+            // lblStudentNameHolder
+            // 
+            this.lblStudentNameHolder.AutoSize = true;
+            this.lblStudentNameHolder.Location = new System.Drawing.Point(27, 35);
+            this.lblStudentNameHolder.Name = "lblStudentNameHolder";
+            this.lblStudentNameHolder.Size = new System.Drawing.Size(37, 13);
+            this.lblStudentNameHolder.TabIndex = 1;
+            this.lblStudentNameHolder.Text = "----------";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(7, 20);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(47, 13);
+            this.lblStudentName.TabIndex = 0;
+            this.lblStudentName.Text = "Nombre:";
+            // 
+            // pbStudentOverview
+            // 
+            this.pbStudentOverview.Location = new System.Drawing.Point(58, 43);
+            this.pbStudentOverview.Name = "pbStudentOverview";
+            this.pbStudentOverview.Size = new System.Drawing.Size(100, 99);
+            this.pbStudentOverview.TabIndex = 1;
+            this.pbStudentOverview.TabStop = false;
+            // 
+            // lblPnlHeader
+            // 
+            this.lblPnlHeader.AutoSize = true;
+            this.lblPnlHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPnlHeader.Location = new System.Drawing.Point(12, 14);
+            this.lblPnlHeader.Name = "lblPnlHeader";
+            this.lblPnlHeader.Size = new System.Drawing.Size(194, 18);
+            this.lblPnlHeader.TabIndex = 0;
+            this.lblPnlHeader.Text = "Datos Generales de Alumno";
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(23, 65);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.Size = new System.Drawing.Size(649, 387);
+            this.dgvStudents.TabIndex = 9;
+            // 
+            // lblStudentId
+            // 
+            this.lblStudentId.AutoSize = true;
+            this.lblStudentId.Location = new System.Drawing.Point(423, 26);
+            this.lblStudentId.Name = "lblStudentId";
+            this.lblStudentId.Size = new System.Drawing.Size(60, 13);
+            this.lblStudentId.TabIndex = 8;
+            this.lblStudentId.Text = "No. Control";
+            // 
+            // txtNoControlFilter
+            // 
+            this.txtNoControlFilter.Location = new System.Drawing.Point(503, 23);
+            this.txtNoControlFilter.Name = "txtNoControlFilter";
+            this.txtNoControlFilter.Size = new System.Drawing.Size(169, 20);
+            this.txtNoControlFilter.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Catalogo de Alumnos";
+            // 
+            // Student_Catalog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 469);
+            this.Controls.Add(this.pnlStudentOverview);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.lblStudentId);
+            this.Controls.Add(this.txtNoControlFilter);
+            this.Controls.Add(this.label2);
+            this.Name = "Student_Catalog";
             this.Text = "Student_Catalog";
+            this.pnlStudentOverview.ResumeLayout(false);
+            this.pnlStudentOverview.PerformLayout();
+            this.gpbStudentOverview.ResumeLayout(false);
+            this.gpbStudentOverview.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStudentOverview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlStudentOverview;
+        private System.Windows.Forms.Button btnStudentDetails;
+        private System.Windows.Forms.Label lblPeriodHolder;
+        private System.Windows.Forms.Label lblPeriod;
+        private System.Windows.Forms.Label lblProyectNameHolder;
+        private System.Windows.Forms.Label lblProyectName;
+        private System.Windows.Forms.GroupBox gpbStudentOverview;
+        private System.Windows.Forms.Label lblNoControlHolder;
+        private System.Windows.Forms.Label lblNoControl;
+        private System.Windows.Forms.Label lblStudentNameHolder;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.PictureBox pbStudentOverview;
+        private System.Windows.Forms.Label lblPnlHeader;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Label lblStudentId;
+        private System.Windows.Forms.TextBox txtNoControlFilter;
+        private System.Windows.Forms.Label label2;
     }
 }
