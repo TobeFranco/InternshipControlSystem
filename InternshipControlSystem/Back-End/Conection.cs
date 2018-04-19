fddf72cbb61fa0ff8ff453ab066d14eb15f6b1df
Tobe Franco <tobe117@outlook.es>
2018-04-18 20:38:41 -0500

    Added Connection Class

---
+++ InternshipControlSystem/Back-End/Conection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace InternshipControlSystem.Back_End
+{
+    class Conection
+    {
+        private static string conectionString = "server=localhost;user=admin;database=internships;port=3306;password=toor";
+        private static MySqlConnection conn;
+
+        private Conection() { }
+
+        public static MySqlConnection getConnection()
+        {
+            if(conn == null)
+            {
+                return new MySqlConnection(conectionString);
+            }
+            return conn;
+        }
+
+    }
+}
