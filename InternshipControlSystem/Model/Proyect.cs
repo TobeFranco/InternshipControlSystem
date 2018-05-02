908a73c75f16ca691ce01c2a90554dcb31b7824e
Tobe Franco <tobe117@outlook.es>
2018-05-01 21:27:40 -0500

    Typo fixed

--- InternshipControlSystem/Model/Proyect.cs
+++ InternshipControlSystem/Model/Proyect.cs
@@ -11,16 +11,18 @@ namespace InternshipControlSystem.Model
         // Variables de Clase
         public int Id;
         public String Name;
-        public int Compan_Id;
+        public int Company_Id;
         public int Student_Id;
+        public string Period;
 
         // Constructor de la clase
-        public Proyect(int Id, String Name, int Company_Id, int Student_Id)
+        public Proyect(int Id, String Name, int Company_Id, int Student_Id, string Period)
         {
             this.Id = Id;
             this.Name = Name;
-            this.Compan_Id = Company_Id;
+            this.Company_Id = Company_Id;
             this.Student_Id = Student_Id;
+            this.Period = Period;
         }
     }
 }
