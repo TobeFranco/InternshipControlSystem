c1ff45031cddc2b46117c66d07b2c19b03773a68
Tobe Franco <tobe117@outlook.es>
2018-05-01 21:32:02 -0500

    Student Details Window Added

---
+++ InternshipControlSystem/Front-End/StudentDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InternshipControlSystem.Model;
+
+namespace InternshipControlSystem.Front_End
+{
+    public partial class StudentDetails : Form
+    {
+        public StudentDetails(Student student)
+        {
+            InitializeComponent();
+            // TODO: Load student info.
+        }
+    }
+}
