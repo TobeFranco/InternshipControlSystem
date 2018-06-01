2d1ece1c94cbba0604b4bf5678082a702848f29b
Tobe Franco <tobe117@outlook.es>
2018-06-01 02:58:13 -0500

    CompanyDao

--- InternshipControlSystem/Back-End/CompanyDao.cs
+++ InternshipControlSystem/Back-End/CompanyDao.cs
@@ -21,12 +21,12 @@ namespace InternshipControlSystem.Back_End
                 MySqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    Company item = new Company(Convert.ToInt32(reader["id"]), Convert.ToString(reader["first_name"]),
-                        Convert.ToString(reader["RFC"]), Convert.ToString(reader["Turn"]),
-                         Convert.ToString(reader["CompanyHome"]), Convert.ToString(reader["CompanyColony"]), Convert.ToString(reader["Cp"])
-                         , Convert.ToString(reader["Fax"]), Convert.ToString(reader["PhoneCompany"]), Convert.ToString(reader["CompanyCity"]), Convert.ToString(reader["TitularName"])
-                         , Convert.ToString(reader["TitularPosition"]), Convert.ToString(reader["AdvisoryName"]), Convert.ToString(reader["AdvisoryPosition"])
-                         , Convert.ToString(reader["AgreedName "]), Convert.ToString(reader["Agreed"]));
+                    Company item = new Company(Convert.ToInt32(reader["id"]), Convert.ToString(reader["comp_name"]),
+                        Convert.ToString(reader["RFC"]), Convert.ToString(reader["turn"]),
+                         Convert.ToString(reader["companyHome"]), Convert.ToString(reader["companyColony"]), Convert.ToString(reader["cp"])
+                         , Convert.ToString(reader["fax"]), Convert.ToString(reader["phoneCompany"]), Convert.ToString(reader["companyCity"]), Convert.ToString(reader["titularName"])
+                         , Convert.ToString(reader["titularPosition"]), Convert.ToString(reader["advisoryName"]), Convert.ToString(reader["advisoryPosition"])
+                         , Convert.ToString(reader["agreedName"]), Convert.ToString(reader["agreed"]));
                     items.Add(item);
                 }
                 reader.Close();
@@ -42,7 +42,7 @@ namespace InternshipControlSystem.Back_End
             return items;
         }
 
-        public static int CreateItem(Company student)
+        public static int CreateItem(Company item)
         {
             MySqlConnection conn = Conection.getConnection();
             try
@@ -50,22 +50,21 @@ namespace InternshipControlSystem.Back_End
                 conn.Open();
                 string sqlStatement = "INSERT INTO companies VALUES(null, @comp_name, @RFC, @Turn, @CompanyHome, @CompanyColony, @Cp, @Fax, @PhoneCompany, @CompanyCity, @TitularName, @TitularPosition, @AdvisoryName, @AdvisoryPosition, @AgreedName, @Agreed); SELECT LAST_INSERT_ID()";
                 MySqlCommand cmd = new MySqlCommand(sqlStatement, conn);
-                cmd.Parameters.AddWithValue("@comp_name", student.Name);
-                cmd.Parameters.AddWithValue("@RFC", student.RFC);              
-                cmd.Parameters.AddWithValue("@Turn", student.TurnDao);
-                cmd.Parameters.AddWithValue("@CompanyHome", student.CompanyHomeDao);            
-                cmd.Parameters.AddWithValue("@CompanyColony", student.CompanyColonyDao);
-                cmd.Parameters.AddWithValue("@Cp", student.CPDao);
-                cmd.Parameters.AddWithValue("@Fax", student.FAXDao);
-                cmd.Parameters.AddWithValue("@PhoneCompany", student.PhoneCompanyDao);
-                cmd.Parameters.AddWithValue("@CompanyCity", student.CompanyCityDao);
-                cmd.Parameters.AddWithValue("@TitularName", student.TitularNameDao);
-                cmd.Parameters.AddWithValue("@TitularPosition", student.TitularPositionDao);
-                cmd.Parameters.AddWithValue("@AdvisoryName", student.AdvisoryNameDao);
-                cmd.Parameters.AddWithValue("@AdvisoryPosition", student.AdvisoryPositionDao);
-                cmd.Parameters.AddWithValue("@AgreedName", student.AgreedNameDao);
-                cmd.Parameters.AddWithValue("@Agreed", student.AgreedDao);
-                cmd.Parameters.AddWithValue("@company_id", student.AgreedDao);
+                cmd.Parameters.AddWithValue("@comp_name", item.Name);
+                cmd.Parameters.AddWithValue("@RFC", item.RFC);              
+                cmd.Parameters.AddWithValue("@Turn", item.TurnDao);
+                cmd.Parameters.AddWithValue("@CompanyHome", item.CompanyHomeDao);            
+                cmd.Parameters.AddWithValue("@CompanyColony", item.CompanyColonyDao);
+                cmd.Parameters.AddWithValue("@Cp", item.CPDao);
+                cmd.Parameters.AddWithValue("@Fax", item.FAXDao);
+                cmd.Parameters.AddWithValue("@PhoneCompany", item.PhoneCompanyDao);
+                cmd.Parameters.AddWithValue("@CompanyCity", item.CompanyCityDao);
+                cmd.Parameters.AddWithValue("@TitularName", item.TitularNameDao);
+                cmd.Parameters.AddWithValue("@TitularPosition", item.TitularPositionDao);
+                cmd.Parameters.AddWithValue("@AdvisoryName", item.AdvisoryNameDao);
+                cmd.Parameters.AddWithValue("@AdvisoryPosition", item.AdvisoryPositionDao);
+                cmd.Parameters.AddWithValue("@AgreedName", item.AgreedNameDao);
+                cmd.Parameters.AddWithValue("@Agreed", item.AgreedDao);
                 return cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -101,20 +100,32 @@ namespace InternshipControlSystem.Back_End
             }
         }
 
-        public static void UpdateItem(Proyect item)
+        public static void UpdateItem(Company item)
         {
             MySqlConnection conn = Conection.getConnection();
             try
             {
                 conn.Open();
-                string sqlStatement = "UPDATE proyects set pro_name=@pro_name, company_id=@company_id, student_id=@student_id" +
-                    "period=@period WHERE id=@id";
+                string sqlStatement = "UPDATE proyects set comp_name=@comp_name, RFC=@RFC, turn=@turn, companyHome=@CompanyHome, companyColony=@companyColony," +
+                    "cp=@Cp, fax=@Fax, phoneCompany=@PhoneCompany, companyCity=@CompanyCity, titularName=@TitularName, titularPosition=@TitularPosition, " +
+                    "advisoryName=@AdvisoryName, advisoryPosition=@AdvisoryPosition, agreedName=@AgreedName, agreed=@Agreed WHERE id=@id";
                 MySqlCommand cmd = new MySqlCommand(sqlStatement, conn);
                 cmd.Parameters.AddWithValue("@id", item.Id);
-                cmd.Parameters.AddWithValue("@pro_name", item.Name);
-                cmd.Parameters.AddWithValue("@company_id", item.Company_Id);
-                cmd.Parameters.AddWithValue("@student_id", item.Student_Id);
-                cmd.Parameters.AddWithValue("@period", item.Period);
+                cmd.Parameters.AddWithValue("@comp_name", item.Name);
+                cmd.Parameters.AddWithValue("@RFC", item.RFC);
+                cmd.Parameters.AddWithValue("@Turn", item.TurnDao);
+                cmd.Parameters.AddWithValue("@CompanyHome", item.CompanyHomeDao);
+                cmd.Parameters.AddWithValue("@CompanyColony", item.CompanyColonyDao);
+                cmd.Parameters.AddWithValue("@Cp", item.CPDao);
+                cmd.Parameters.AddWithValue("@Fax", item.FAXDao);
+                cmd.Parameters.AddWithValue("@PhoneCompany", item.PhoneCompanyDao);
+                cmd.Parameters.AddWithValue("@CompanyCity", item.CompanyCityDao);
+                cmd.Parameters.AddWithValue("@TitularName", item.TitularNameDao);
+                cmd.Parameters.AddWithValue("@TitularPosition", item.TitularPositionDao);
+                cmd.Parameters.AddWithValue("@AdvisoryName", item.AdvisoryNameDao);
+                cmd.Parameters.AddWithValue("@AdvisoryPosition", item.AdvisoryPositionDao);
+                cmd.Parameters.AddWithValue("@AgreedName", item.AgreedNameDao);
+                cmd.Parameters.AddWithValue("@Agreed", item.AgreedDao);
                 cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -128,69 +139,37 @@ namespace InternshipControlSystem.Back_End
         }
 
 
-        //public static Company GetItem(int id)
-        //{
-        //    MySqlConnection conn = Conection.getConnection();
-        //   Company item = null;
-        //    try
-        //    {
-        //        conn.Open();
-        //        string sqlStatement = "SELECT * FROM companies WHERE id = @id";
-        //        MySqlCommand cmd = new MySqlCommand(sqlStatement, conn);
-        //        cmd.Parameters.AddWithValue("@id", id);
-        //        MySqlDataReader reader = cmd.ExecuteReader();
-        //        if (reader.Read())
-        //        {
-        //            Company item = new Company(Convert.ToInt32(reader["id"]), Convert.ToString(reader["first_name"]),
-        //                       Convert.ToString(reader["RFC"]), Convert.ToInt32(reader["company_assessor_id"]), Convert.ToString(reader["Turn"]),
-        //                        Convert.ToString(reader["CompanyHome"]), Convert.ToString(reader["CompanyColony"]), Convert.ToString(reader["Cp"])
-        //                        , Convert.ToString(reader["Fax"]), Convert.ToString(reader["PhoneCompany"]), Convert.ToString(reader["CompanyCity"]), Convert.ToString(reader["TitularName"])
-        //                        , Convert.ToString(reader["TitularPosition"]), Convert.ToString(reader["AdvisoryName"]), Convert.ToString(reader["AdvisoryPosition"])
-        //                        , Convert.ToString(reader["AgreedName "]), Convert.ToString(reader["Agreed"]));
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.ToString());
-        //    }
-        //    finally
-        //    {
-        //        conn.Close();
-        //    }
-        //    return item;
-        //}
-
-        //public static Company GetItem(Student student)
-        //{
-        //    MySqlConnection conn = Conection.getConnection();
-        //    Company item = null;
-        //    try
-        //    {
-        //        conn.Open();
-        //        string sqlStatement = "SELECT * FROM proyects WHERE student_id = @student_id";
-        //        MySqlCommand cmd = new MySqlCommand(sqlStatement, conn);
-        //        cmd.Parameters.AddWithValue("@student_id", student.Id);
-        //        MySqlDataReader reader = cmd.ExecuteReader();
-        //        if (reader.Read())
-        //        {
-        //            Company item = new Company(Convert.ToInt32(reader["id"]), Convert.ToString(reader["first_name"]),
-        //                Convert.ToString(reader["RFC"]), Convert.ToInt32(reader["company_assessor_id"]), Convert.ToString(reader["Turn"]),
-        //                 Convert.ToString(reader["CompanyHome"]), Convert.ToString(reader["CompanyColony"]), Convert.ToString(reader["Cp"])
-        //                 , Convert.ToString(reader["Fax"]), Convert.ToString(reader["PhoneCompany"]), Convert.ToString(reader["CompanyCity"]), Convert.ToString(reader["TitularName"])
-        //                 , Convert.ToString(reader["TitularPosition"]), Convert.ToString(reader["AdvisoryName"]), Convert.ToString(reader["AdvisoryPosition"])
-        //                 , Convert.ToString(reader["AgreedName "]), Convert.ToString(reader["Agreed"]));
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.ToString());
-        //    }
-        //    finally
-        //    {
-        //        conn.Close();
-        //    }
-        //    return item;
-        //}
+        public static Company GetItem(int id)
+        {
+            MySqlConnection conn = Conection.getConnection();
+            Company item = null;
+            try
+            {
+                conn.Open();
+                string sqlStatement = "SELECT * FROM companies WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(sqlStatement, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    item = new Company(Convert.ToInt32(reader["id"]), Convert.ToString(reader["comp_name"]),
+                        Convert.ToString(reader["RFC"]), Convert.ToString(reader["turn"]),
+                         Convert.ToString(reader["companyHome"]), Convert.ToString(reader["companyColony"]), Convert.ToString(reader["cp"])
+                         , Convert.ToString(reader["fax"]), Convert.ToString(reader["phoneCompany"]), Convert.ToString(reader["companyCity"]), Convert.ToString(reader["titularName"])
+                         , Convert.ToString(reader["titularPosition"]), Convert.ToString(reader["advisoryName"]), Convert.ToString(reader["advisoryPosition"])
+                         , Convert.ToString(reader["agreedName"]), Convert.ToString(reader["agreed"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return item;
+        }
 
     }
 }
